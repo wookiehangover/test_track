2251792637fea7345fad8e2021a5243edfe139de
Sam Breed <sam@quickleft.com>
2011-08-29 16:45:14 -0600

    adding config w/ dynamic reload

--- lib/js_test_gem.rb
+++ lib/js_test_gem.rb
@@ -1,4 +1,31 @@
-require 'js_test_gem/engine'
+require 'active_support/core_ext/module/attribute_accessors'
 
 module JsTestGem
+
+	mattr_accessor :custom_config
+	@@custom_config = false
+
+	mattr_accessor :jasmine_manifest
+	@@jasmine_manifest = "jasmine"
+
+	mattr_accessor :qunit_manifest
+	@@qunit_manifest = "qunit"
+
+	mattr_accessor :application_manifest
+	@@application_manifest = "application"
+
+	mattr_accessor :jasmine_path
+	@@jasmine_path = "spec/javascripts"
+
+	mattr_accessor :qunit_path
+	@@qunit_path = "test/javascripts"
+
+	def self.setup
+		@@custom_config = true
+		yield self
+		require 'js_test_gem/engine'
+	end
+
+	require 'js_test_gem/engine' unless @@custom_config
 end
+
