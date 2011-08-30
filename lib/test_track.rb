4054f7500f3ccb859b85a09e920c548b8be720d7
Ryan Cook <ryan@quickleft.com>
2011-08-30 10:57:17 -0600

    remove all that funky custom config jazz

--- lib/test_track.rb
+++ lib/test_track.rb
@@ -2,30 +2,26 @@ require 'active_support/core_ext/module/attribute_accessors'
 
 module TestTrack
 
-	mattr_accessor :custom_config
-	@@custom_config = false
+  mattr_accessor :jasmine_manifest
+  @@jasmine_manifest = "jasmine"
 
-	mattr_accessor :jasmine_manifest
-	@@jasmine_manifest = "jasmine"
+  mattr_accessor :qunit_manifest
+  @@qunit_manifest = "qunit"
 
-	mattr_accessor :qunit_manifest
-	@@qunit_manifest = "qunit"
+  mattr_accessor :application_manifest
+  @@application_manifest = "application"
 
-	mattr_accessor :application_manifest
-	@@application_manifest = "application"
+  mattr_accessor :jasmine_path
+  @@jasmine_path = "spec/javascripts"
 
-	mattr_accessor :jasmine_path
-	@@jasmine_path = "spec/javascripts"
+  mattr_accessor :qunit_path
+  @@qunit_path = "test/javascripts"
 
-	mattr_accessor :qunit_path
-	@@qunit_path = "test/javascripts"
+  def self.setup
+    yield self
+  end
 
-	def self.setup
-		@@custom_config = true
-		yield self
-		require 'test_track/engine'
-	end
-
-	require 'test_track/engine' unless @@custom_config
 end
+
+require 'test_track/engine'
 
